f349b46f8877a58beb8da832e5a2f088e909b59f
Aleksandr Kolesov <aleksandrkolesov@Aleksandrs-MacBook-Pro.local>
2025-11-15 16:49:06 +0100

    fixing build

--- next.config.ts
+++ next.config.ts
@@ -6,50 +6,6 @@ const nextConfig: NextConfig = {
 	pageExtensions: ["tsx", "ts", "jsx", "js"].map((ext) => {
 		return ext;
 	}),
-	webpack(config, { webpack }) {
-		// Exclude Storybook files from the build
-		config.module.rules.push({
-			test: /\.(stories|story)\.(tsx?|jsx?)$/,
-			loader: "ignore-loader",
-		});
-
-		// Exclude .mdx files used by Storybook
-		config.module.rules.push({
-			test: /\.mdx$/,
-			loader: "ignore-loader",
-		});
-		// Webpack config for when turbopack is disabled
-		// Find the existing rule that handles SVG imports
-		// biome-ignore lint/suspicious/noExplicitAny: Webpack config types are complex
-		const fileLoaderRule = config.module.rules.find((rule: any) =>
-			rule.test?.test?.(".svg"),
-		);
-
-		config.module.rules.push(
-			// Reapply the existing rule, but only for svg imports ending in ?url
-			{
-				...fileLoaderRule,
-				test: /\.svg$/i,
-				resourceQuery: /url/, // *.svg?url
-			},
-			// Convert all other *.svg imports to React components
-			{
-				test: /\.svg$/i,
-				issuer: fileLoaderRule.issuer,
-				resourceQuery: {
-					not: [...(fileLoaderRule.resourceQuery?.not || []), /url/],
-				}, // exclude if *.svg?url
-				use: ["@svgr/webpack"],
-			},
-		);
-
-		// Modify the file loader rule to ignore *.svg, since we have it handled now.
-		if (fileLoaderRule) {
-			fileLoaderRule.exclude = /\.svg$/i;
-		}
-
-		return config;
-	},
 };
 
 export default nextConfig;
